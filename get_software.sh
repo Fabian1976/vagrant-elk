505cf87129df96465ee51e2c63156c9bf45d1b69
Fabian van der Hoeven <fabian.vanderhoeven@vermont24-7.com>
2019-08-15 08:24:56 +0200

    Added script to download additional software which is not available in repo's

---
+++ get_software.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cerebro_version='0.8.3'
+
+wget https://github.com/lmenezes/cerebro/releases/download/v${cerebro_version}/cerebro-${cerebro_version}.tgz -O software/cerebro-${cerebro_version}.tgz
